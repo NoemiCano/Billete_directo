5170ef801eb895863b0f2acb7705acc1f084be1f
Laura <lauraotero005@gmail.com>
2026-05-08 12:11:18 +0200

    Update loading animation

--- script.js
+++ script.js
@@ -37,3 +37,18 @@ document.querySelectorAll('.nav__text, .only-mobile').forEach(n => n.addEventLis
 }));
 
 // #endregion
+
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+
+    });
+});
+
+hiddenElements.forEach((el) => observer.observe(el));
