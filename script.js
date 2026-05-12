937fcb823ce3f4f3845cfe047d7a34fa1f9b1c05
NoemiCano <noemicanoconesa@gmail.com>
2026-05-12 14:20:27 +0200

    Fixed email formsubmit

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function() {
         }
         
         // Caso especial para la página de inicio (si la ruta es solo "/")
-        if (currentPath === "/" && link.getAttribute('href') === "index_l.html") {
+        if (currentPath === "/" && link.getAttribute('href') === "index.html") {
             link.classList.add('active');
         }
     });
